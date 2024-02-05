da6361ba161897890896fbd74d6b4f847e834a0f
williamfisher9 <fisher.a.william@gmail.com>
2024-02-04 23:06:11 -0600

    initial code

---
+++ src/main/java/com/apps/interceptors/InterceptorsApplication.java
@@ -0,0 +1,13 @@
+package com.apps.interceptors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterceptorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterceptorsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apps/interceptors/InterceptorsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apps.interceptors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterceptorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
